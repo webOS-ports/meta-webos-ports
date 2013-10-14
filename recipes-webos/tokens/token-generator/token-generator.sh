51a4772e3861d9842b812d03b915e4c91546c1f1
Simon Busch <morphis@gravedo.de>
2013-10-14 08:14:40 +0200

    token-generator: add systemd support and rework token creation
    Signed-off-by: Simon Busch <morphis@gravedo.de>
    Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

--- recipes-webos/tokens/token-generator/token-generator.sh
+++ recipes-webos/tokens/token-generator/token-generator.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
-if [ ! -d /dev/tokens ] ; then
-    mkdir -p /dev/tokens
+TOKENS_DIR=/var/tokens
+TOKENS_TARGET_DIR=/dev/tokens
+
+if [ ! -d $TOKENS_DIR ] ; then
+    mkdir -p $TOKENS_DIR
 fi
 
 for token in WIFIoADDR BToADDR; do
-    touch /dev/tokens/${token}
+    if [ ! -e $TOKENS_DIR/$token ] ; then
+        touch $TOKENS_DIR/$token
+    fi
 done
 
+if [ ! -e $TOKENS_DIR/nduid ] ; then
+    nduid=`dd if=/dev/urandom bs=1024 count=1 | md5sum | cut -d' ' -f 1`
+    echo $nduid > $TOKENS_DIR/nduid
+fi
+
+mkdir -p $TOKENS_TARGET_DIR
+mount -o bind,ro $TOKENS_DIR $TOKENS_TARGET_DIR
