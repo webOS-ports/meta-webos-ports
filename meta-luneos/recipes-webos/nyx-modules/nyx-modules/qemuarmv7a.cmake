ff891c7087eb84cb1d71bd7825bd35d7468c69f5
Herman van Hazendonk <github.com@herrie.org>
2019-07-11 10:16:11 +0200

    nyx-modules: Update copyright
    Update the copyrights.
    
    Signed-off-by: Herman van Hazendonk <github.com@herrie.org>

--- meta-luneos/recipes-webos/nyx-modules/nyx-modules/qemuarmv7a.cmake
+++ meta-luneos/recipes-webos/nyx-modules/nyx-modules/qemuarmv7a.cmake
@@ -1,6 +1,6 @@
 # @@@LICENSE
 #
-#      Copyright (c) 2010-2013 LG Electronics, Inc.
+#      Copyright (c) 2010-2019 LG Electronics, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
