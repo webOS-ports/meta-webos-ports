57854349389cbf1bbe74dec46b68d8dd6fb95e9c
Herman van Hazendonk <github.com@herrie.org>
2021-04-29 10:00:46 +0200

    meta-webos-ports: Update mido target for Halium 9.0
    Use bluebinder for bluetooth and native vibrator module instead of hybris one.
    
    Signed-off-by: Herman van Hazendonk <github.com@herrie.org>

--- meta-luneos/recipes-webos/nyx-modules/nyx-modules/mido.cmake
+++ meta-luneos/recipes-webos/nyx-modules/nyx-modules/mido.cmake
@@ -24,12 +24,12 @@ set(NYXMOD_OW_CHARGER			TRUE)
 set(NYXMOD_OW_KEYS			TRUE)
 set(NYXMOD_OW_TOUCHPANEL		FALSE)
 set(NYXMOD_OW_TOUCHPANEL_MTDEV		TRUE)
+set(NYXMOD_OW_HAPTICS			TRUE)
 
 # provided by nyx-modules-hybris
 set(NYXMOD_OW_DEVICEINFO		FALSE)
 set(NYXMOD_OW_SYSTEM			FALSE)
 set(NYXMOD_OW_LED			FALSE)
-set(NYXMOD_OW_HAPTICS			FALSE)
 
 add_definitions(-DBATTERY_SYSFS_PATH=\"/sys/class/power_supply/battery/\")
 add_definitions(-DTOUCHPANEL_DEVICE=\"/dev/input/event1\")
